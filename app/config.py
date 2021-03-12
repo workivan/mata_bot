26d85237e0d52014b224eee3fe99b9c946247412
gwenbleyd <johncount94@gmail.com>
2021-03-12 18:55:33 +0300

    edit gitignore

---
+++ app/config.py
@@ -0,0 +1,10 @@
+from aiogram import Bot
+
+SUPER_USER_NICKNAME = 'gwenbleyd'
+TOKEN = '1652503829:AAEO9y_OuSiw3bGzNMG2sehrH8HjD5DrUu0'
+SUPER_USER_LOGIN = 'gwenbleyd'
+PASSWORD = '841XcTNeYD'
+SUPER_USER_ID = 447959709
+PIN_MESSAGE = 'Это первое закрепленное сообщение для проверки'
+bot = Bot(token=TOKEN)
+USERS = dict()
