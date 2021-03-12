f87719bc9d8e9cecda18a64af5c98e9744e4b21c
gwenbleyd <johncount94@gmail.com>
2021-03-12 14:57:31 +0300

    the telebot library was changed to the aiogram framework, logging was added, login to the chat by password, checking the uniqueness of the nickname

--- main.py
+++ main.py
@@ -1,100 +1,177 @@
-import telebot
-from user import User
-from config import TOKEN, SUPER_USER_ID, SUPER_USER_LOGIN, SUPER_USER_NICKNAME
+import asyncio
+import logging
 
-bot = telebot.TeleBot(TOKEN)
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from user import User
+from config import *
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+logger = logging.getLogger(__name__)
 
 users = dict()
-
 super_user = User(SUPER_USER_NICKNAME, SUPER_USER_LOGIN, True, False, True, True)
 users.update({SUPER_USER_ID: super_user})
 
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id,
-                     'Привет. Я бот. Вот правила:')
+class Register(StatesGroup):
+    password = State()
+    nickname = State()
 
 
-# @bot.message_handler(commands=['admin'])
-# def console_admin(message):
-#     pass
+class AdminState(StatesGroup):
+    add = State()
+    delete = State()
 
 
-@bot.message_handler(commands='delete_user')
-def delete_user(message):
-    if message.chat.id in users:
-        msg = bot.send_message(message.chat.id, 'Введи никнейм того, кого хочешь удалить')
-        bot.register_next_step_handler(msg, process_delete_user_step)
+class UserState(StatesGroup):
+    delete = State()
 
 
-@bot.message_handler(commands=['register_admin'])
-def register_admin(message):
-    if int(message.chat.id) == SUPER_USER_ID:
-        msg = bot.reply_to(message, "Введи никнейм того, кого хочешь сделать админстратором")
-        bot.register_next_step_handler(msg, process_add_admin_step)
+@dp.message_handler(commands=['start', 'help'])
+async def start_message(message: types.Message):
+    await message.reply(
+        'Привет. Я твой бот для анонимного общения. \n'
+        'Введи /register, чтобы зарегистрироваться \n'
+        'Правила: \n'
+    )
 
 
-@bot.message_handler(commands=['delete_admin'])
-def delete_admin(message):
-    if int(message.chat.id) == SUPER_USER_ID:
-        msg = bot.reply_to(message, "Введи никнейм того, кого хочешь удалить из админстратором")
-        bot.register_next_step_handler(msg, process_delete_admin_step)
-
-
-@bot.message_handler(commands=['register'])
-def register(message):
-    if message.chat.id in users:
-        bot.send_message(message.chat.id, 'Ты уже зарегистрирован!')
-    else:
-        msg = bot.reply_to(message, "Введи свой никнейм")
-        bot.register_next_step_handler(msg, process_nickname_step)
-
-
-def process_add_admin_step(message):
+@dp.message_handler(commands='delete_user')
+async def delete_user(message: types.Message):
     for user_key, user_value in users.items():
-        if user_value.nickname == message.text:
-            user_value.is_admin = True
-            bot.send_message(message.chat.id, 'Новый администратор добавлен')
-            bot.send_message(user_key, 'Теперь ты администратор')
+        if user_key == message.chat.id and user_value.is_admin == True:
+            await message.reply('Введи никнейм того, кого хочешь удалить')
+            await UserState.delete.set()
+        else:
+            await message.reply('Ты не можешь банить пользователей')
 
 
-def process_delete_user_step(message):
+@dp.message_handler(state=UserState.delete)
+async def process_delete_user_step(message: types.Message, state: FSMContext):
     for user_key, user_value in users.items():
         if user_value.nickname == message.text:
             user_value.banned = True
-            bot.send_message(message.chat.id, 'Пользователь удален')
-            bot.send_message(user_key, 'Ты забанен в чате')
+            await message.answer('Пользователь удален!')
+            await bot.send_message(user_key, 'Ты забанен в чате!')
+            await state.finish()
+            return
+    await message.answer('Человека с таким никнеймом нет в чате')
 
 
-def process_delete_admin_step(message):
+@dp.message_handler(commands=['register_admin'])
+async def register_admin(message: types.Message):
+    if int(message.chat.id) == SUPER_USER_ID:
+        await message.reply('Введи никнейм того, кого хочешь сделать администратором')
+        await AdminState.add.set()
+    else:
+        await bot.send_message(message.chat.id, 'Ты не можешь добавлять новых администраторов')
+
+
+@dp.message_handler(state=AdminState.add)
+async def process_add_admin_step(message: types.Message, state: FSMContext):
+    for user_key, user_value in users.items():
+        if user_value.nickname == message.text:
+            user_value.is_admin = True
+            await message.answer('Новый администратор добавлен!')
+            await bot.send_message(user_key, 'Теперь ты администратор!')
+            await state.finish()
+            return
+    await message.answer('Человека с таким никнеймом нет в чате')
+
+
+@dp.message_handler(commands=['delete_admin'])
+async def delete_admin(message: types.Message):
+    if int(message.chat.id) == SUPER_USER_ID:
+        await message.reply('Введи никнейм того, кого хочешь удалить из админстратором')
+        await AdminState.delete.set()
+    else:
+        await bot.send_message(message.chat.id, 'Ты не можешь удалять администраторов')
+
+
+@dp.message_handler(state=AdminState.delete)
+async def process_delete_admin_step(message: types.Message, state: FSMContext):
     for user_key, user_value in users.items():
         if user_value.nickname == message.text:
             user_value.is_admin = False
-            bot.send_message(message.chat.id, 'Администратор удален')
-            bot.send_message(user_key, 'Ты удален из администраторов')
+            await message.answer('Администратор удален!')
+            await bot.send_message(user_key, 'Ты удален из администраторов!')
+            await state.finish()
+            return
+    await message.answer('Человека с таким никнеймом нет в чате')
 
 
-def process_nickname_step(message):
+@dp.message_handler(commands=['register'])
+async def register(message: types.Message):
+    if message.chat.id in users:
+        await message.reply('Ты уже зарегистрирован!')
+    else:
+        await message.reply('Введи пароль для продолжения регистрации')
+        await Register.password.set()
+
+
+@dp.message_handler(state=Register.password)
+async def process_password_step(message: types.Message):
+    if PASSWORD == message.text:
+        await Register.next()
+        await message.reply("Верный пороль. Теперь введи никнейм!")
+    else:
+        await message.reply('Пороль не тот, ты не можешь войти в чат!')
+
+
+@dp.message_handler(state=Register.nickname)
+async def process_nickname_step(message: types.Message, state: FSMContext):
+    for user_key, user_value in users.items():
+        if user_value.nickname == message.text:
+            await message.reply("Этот никнейм уже занят. Выбери другой!")
+            return
     user_id = message.chat.id
     nickname = message.text
     username = message.chat.username
     users.update({user_id: User(nickname, username)})
+    await message.reply("Никнейм свободен. Можешь начать общение!")
+    await state.finish()
 
 
-@bot.message_handler(content_types=['text'])
-def send_message(message):
-    for user_key, user_value in users.items():
-        if user_key != message.chat.id and user_value.subscription is True:
-            bot.send_message(user_key, message.text)
+@dp.message_handler(content_types=['text'])
+async def send_message(message: types.Message):
+    if not users.get(message.chat.id).banned:
+        for user_key, user_value in users.items():
+            if user_key != message.chat.id and user_value.subscription == True:
+                if not user_value.banned:
+                    await bot.send_message(user_key, message.text)
+    else:
+        await message.reply('Ты в бане!')
 
 
-@bot.message_handler(content_types=["sticker", "pinned_message", "photo", "audio", "document", "video", "video_note", "voice", "location", "contact"])
-def send_image_error(message):
-    bot.send_message(message.chat.id, 'Прости, но по правилам ты можешь отправлять только текстовые сообщения не чаще,'
-                                      'чем раз в минуту')
-    bot.delete_message(message.chat.id, message.message_id)
+@dp.message_handler(content_types=["sticker", "pinned_message", "photo", "audio", "document", "video", "video_note",
+                                   "voice", "location", "contact"])
+async def send_image_error(message: types.Message):
+    if message.chat.id == SUPER_USER_ID:
+        for user_key, user_value in users.items():
+            if user_key != message.chat.id and user_value.subscription == True:
+                await bot.send_message(user_key, message.text)
+    else:
+        await message.reply('Прости, но по правилам ты можешь отправлять только текстовые сообщения '
+                            'не чаще, чем раз в 30 секунд')
+        await bot.delete_message(message.chat.id, message.message_id)
 
+
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    )
+    logger.info("Starting bot")
+
+    try:
+        await dp.start_polling()
+    finally:
+        await bot.close()
 
 if __name__ == '__main__':
-    bot.infinity_polling()
+    asyncio.run(main())
